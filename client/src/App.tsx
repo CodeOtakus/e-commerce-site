87f40c9a088f77809dadff272d417dbd91a8a224
ichigo_kurosaki <125087781+ichigo-k@users.noreply.github.com>
2024-02-22 15:00:37 +0000

    Refactor routes in App component and added suspense

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,23 @@
-import { Route, Routes, } from "react-router-dom"
-import { lazy , Suspense} from "react"
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-const Layout = lazy(() => import('./views/layouts/Layout'))
-const Page = lazy(() => import('./views/pages/Page'))
-
-
-
+const Layout = lazy(() => import('./views/layouts/Layout'));
+const Page = lazy(() => import('./views/pages/Page'));
 
 function App() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Page name={"Home"} />} />
-          <Route path="/dashboard" element={<Page name={"dashboard"} />} />
+          <Route index element={<Page name={"Home"} />} />
+          <Route path="/dashboard" element={<Page name={"Dashboard"} />} />
           <Route path="/store" element={<Page name={"Store"} />} />
           <Route path="/category" element={<Page name={"Category"} />} />
-          <Route path="/products" element={<Page  name={"Products"}/>} />
+          <Route path="/products" element={<Page name={"Products"} />} />
         </Route>
       </Routes>
-
-    </>
-    
-  )
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
