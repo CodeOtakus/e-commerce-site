44073a533a734391d3aea049276d2ba055929657
ichigo_kurosaki <125087781+ichigo-k@users.noreply.github.com>
2024-02-22 14:55:46 +0000

    Add react-router-dom and lazy loading for routes also Added custom routes and file structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,27 @@
+import { Route, Routes, } from "react-router-dom"
+import { lazy , Suspense} from "react"
+
+const Layout = lazy(() => import('./views/layouts/Layout'))
+const Page = lazy(() => import('./views/pages/Page'))
+
+
 
 
 function App() {
   return (
-    <div className="text-red-600">App</div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Page name={"Home"} />} />
+          <Route path="/dashboard" element={<Page name={"dashboard"} />} />
+          <Route path="/store" element={<Page name={"Store"} />} />
+          <Route path="/category" element={<Page name={"Category"} />} />
+          <Route path="/products" element={<Page  name={"Products"}/>} />
+        </Route>
+      </Routes>
+
+    </>
+    
   )
 }
 
