7331bd0fb80ece9710c96f3a0ef9404ac5a2cc1c
ichigo_kurosaki <125087781+ichigo-k@users.noreply.github.com>
2024-02-23 18:29:42 +0000

    Update Dashboard chart colors

--- client/src/views/pages/Dashboard.tsx
+++ client/src/views/pages/Dashboard.tsx
@@ -19,8 +19,8 @@ export default function Dashboard() {
         <DashboardCards name={"Stores"} value={"300"} icon={<LuBox />} />
        </div>
 
-       <div className='mt-5 w-full flex justify-between max-lg:flex-col max-lg:space-y-3 lg:space-x-3'>
-        <div className='bg-white lg:w-1/3 w-full lg:h-[25rem] p-5 rounded-md shadow-lg'>
+       <div className='mt-5 w-full flex justify-between max-lg:flex-col-reverse  max-lg:space-y-5 lg:space-x-3'>
+        <div className='bg-white lg:w-1/3 w-full lg:h-[25rem] p-5 rounded-md shadow-lg max-lg:mt-5'>
           <Doughnut 
             data={{
               labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
@@ -29,20 +29,20 @@ export default function Dashboard() {
                   label: 'Revenue',
                   data: [12, 19, 3, 5, 2, 3],
                   backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
+                    '#fb8500',
+                    '#ff9500',
+                    '#ffa200',
+                    '#ffb700',
+                    '#ffd000',
+                    '#ffea00',
                   ],
                   borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
+                    '#fb8500',
+                    '#ff9500',
+                    '#ffa200',
+                    '#ffb700',
+                    '#ffd000',
+                    '#ffea00',
                   ],
                   borderWidth: 1,
                 },
@@ -62,22 +62,8 @@ export default function Dashboard() {
               {
                 label: 'Revenue',
                 data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)',
-                ],
-                borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)',
-                ],
+                backgroundColor:"#fb8500",
+                borderColor: "#fb8500",
                 borderWidth: 1,
               },
             ],
