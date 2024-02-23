1b8f958d085a2a6ee06a0f4f09fb066c7653e7ac
ichigo_kurosaki <125087781+ichigo-k@users.noreply.github.com>
2024-02-23 20:44:43 +0000

    Add Store page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { lazy, Suspense } from "react";
 const Layout = lazy(() => import('./views/layouts/Layout'));
 const Page = lazy(() => import('./views/layouts/Page'));
 const Dashboard = lazy(() => import('./views/pages/Dashboard'));
+const Store = lazy(() => import('./views/pages/storePages/Store'));
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Navigate to ="/dashboard" />}  />
           <Route path="/dashboard" element={<Page page={<Dashboard />} />} />
-          <Route path="/store" element={<Page page={"Store"} />} />
+          <Route path="/store" element={<Page page={<Store />} />} />
           <Route path="/category" element={<Page page={"Category"} />} />
           <Route path="/products" element={<Page page={"Products"} />} />
           <Route path="*" element={<Page page={"Not found"} />} />
--- client/src/views/pages/Dashboard.tsx
+++ client/src/views/pages/Dashboard.tsx
@@ -12,7 +12,7 @@ export default function Dashboard() {
     <div className='flex flex-col gap-4'>
         <h1 className='text-3xl text-gray-600'> Welcome <span className='text-orange-500'>Kephas</span></h1>
 
-       <div className='w-full mt-5 flex justify-between md:space-x-4 max-md:flex-col max-md:space-y-4 '>
+       <div className='w-full mt-5 flex justify-between md:space-x-4 max-md:flex-col max-md:space-y-4  '>
         <DashboardCards name={"Revenue"} value={"$ 87,000"} icon={<FiTag />} />
         <DashboardCards name={"Profit"} value={"$ 45,000"} icon={<LuBadgeDollarSign/>} />
         <DashboardCards name={"Products"} value={"40,000"} icon={<LuLayoutGrid />} />
@@ -20,7 +20,7 @@ export default function Dashboard() {
        </div>
 
        <div className='mt-5 w-full flex justify-between max-lg:flex-col-reverse  max-lg:space-y-5 lg:space-x-3'>
-        <div className='bg-white lg:w-1/3 w-1/2 lg:h-[25rem] p-5 rounded-md shadow-lg max-lg:mt-5 h-[20rem]'>
+        <div className='bg-white lg:w-1/3 md:w-1/2 w-full lg:h-[25rem] p-5 rounded-md shadow-lg max-lg:mt-5 h-[20rem]'>
           <Doughnut 
             data={{
               labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
