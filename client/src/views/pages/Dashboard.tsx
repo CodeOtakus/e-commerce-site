44f17357d809122633cc40f9d15a90f1be6be1a5
ichigo_kurosaki <125087781+ichigo-k@users.noreply.github.com>
2024-02-23 18:16:18 +0000

    Add ChartJS and react-chartjs-2 for revenue visualization also Finally comleted dashboard page

--- client/src/views/pages/Dashboard.tsx
+++ client/src/views/pages/Dashboard.tsx
@@ -1,6 +1,11 @@
 import DashboardCards from '../components/DashboardCards'
 import { FiTag } from "react-icons/fi";
 import { LuLayoutGrid, LuBox , LuBadgeDollarSign } from "react-icons/lu";
+import { Chart as ChartJS, registerables ,Colors} from 'chart.js';
+ChartJS.register(Colors);
+ChartJS.register(...registerables);
+
+import { Bar , Doughnut } from 'react-chartjs-2'
 
 export default function Dashboard() {
   return (
@@ -16,10 +21,68 @@ export default function Dashboard() {
 
        <div className='mt-5 w-full flex justify-between max-lg:flex-col max-lg:space-y-3 lg:space-x-3'>
         <div className='bg-white lg:w-1/3 w-full lg:h-[25rem] p-5 rounded-md shadow-lg'>
-
+          <Doughnut 
+            data={{
+              labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+              datasets: [
+                {
+                  label: 'Revenue',
+                  data: [12, 19, 3, 5, 2, 3],
+                  backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                  ],
+                  borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                  ],
+                  borderWidth: 1,
+                },
+              ],
+            
+            }}
+          />
+        
         </div>
-        <div className='bg-white lg:w-2/3 w-full lg:h-[25rem] p-5 rounded-md shadow-lg '>
 
+
+        <div className='bg-white lg:w-2/3 w-full lg:h-[25rem] p-5 rounded-md shadow-lg '>
+        <Bar
+          data={{
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+            datasets: [
+              {
+                label: 'Revenue',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+              },
+            ],
+          }}
+         />
         </div>
        </div>
     </div>
